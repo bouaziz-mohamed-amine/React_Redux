e2ae6681602fb1876b0b935f1ce4717608fa452a
DELL <amine.m.a1998@gmail.com>
2021-06-30 10:34:30 +0100

    first push

--- src/app/store.js
+++ src/app/store.js
@@ -3,15 +3,32 @@ import counter from '../reducers/counter';
 import favoriteReducer from '../reducers/favoritesreducer'
 
 
-
-const devtools=window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
-
-const store =createStore(combineReducers({
-    favorites : favoriteReducer,
-    counter : counter,
-}),devtools)
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 
 
-export default store  ;
+    const devtools=window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+    const reducers=combineReducers({
+        favorites : favoriteReducer,
+        counter : counter,
+    });
+
+    const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['favorites'],
+    blackList : ["counter"]
+    }
+
+  const persistedReducer = persistReducer(persistConfig,reducers);
+
+  //const store =createStore(reducers,devtools)
+
+
+
+export let store = createStore(persistedReducer,devtools) ;
+export let persistor = persistStore(store) ;
+
+ 
--- src/index.js
+++ src/index.js
@@ -4,17 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
-import store from './app/store';
-import {
-  BrowserRouter 
-  
-} from "react-router-dom";
+
+import {  BrowserRouter } from "react-router-dom";
+import { PersistGate } from 'redux-persist/integration/react'
+import  {store,persistor}    from './app/store';
+
 
 ReactDOM.render(
   <React.StrictMode>
    <Provider store={store} >
         <BrowserRouter>
-          <App />
+        <PersistGate  loading={null} persistor={persistor} >
+                <App />
+        </PersistGate>
         </BrowserRouter>
    </Provider> 
   </React.StrictMode>,
